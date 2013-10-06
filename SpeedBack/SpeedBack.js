acc5b2005e72abfd70ef3e618aa588694da65058
maniarathi <mani.arathi@gmail.com>
2013-10-05 23:19:18 -0400

    added additional tabs to create new events

--- SpeedBack/SpeedBack.js
+++ SpeedBack/SpeedBack.js
@@ -2,29 +2,19 @@ if (Meteor.isClient) {
   Template.title.greeting = function () {
     $('#tabs').tab();
   };
-  
-  Meteor.loginWithGithub({
-  requestPermissions: ['user', 'public_repo']
-  }, function (err) {
-  if (err)
-    Session.set('errorMessage', err.reason || 'Unknown error');
-	});
-  
+
   Template.register.events({
-	'submit #login-form' : function (e,t) {
+	"click button[name='register']" : function (e,t) {
 		e.preventDefault();
-		var	user	= t.find('#login-username').value(),
-			email	= t.find('#login-email').value(),
-			password= t.find('#login-password').value();
-		Accounts.createUser({email: email, password : password}, function(err) {
-          if (err) {
-            // Inform the user that account creation failed
-			alert('This account has already been created.  Please try logging in.');
-          } else {
-            // Success. Account has been created and the user
-            // has logged in successfully. 
-			alert('Success!');
-          }
+		var	user	= t.find('#login-username').value,
+			email	= t.find('#login-email').value,
+			password= t.find('#login-password').value;
+			Meteor.call("createUser", getSessionToken(), user, email, password, function (error, result) {
+			if (!error) {
+				alert("User " + name + " added successfully.");
+			} else {
+				alert(error.reason);
+			}
         });
       return false;
 	}
